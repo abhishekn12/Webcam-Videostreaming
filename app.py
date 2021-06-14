40a313587ca7f40c7c03421d67f24be8228c5a3e
Abhishek Nagesh <abhishek.n@daimler.com>
2021-06-14 23:42:54 +0530

    third

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ import cv2
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)
 
+def generate_frames():
+    while True:
+        success,frame = camera.read()  #frame
+        if not success:
+            break
+        else:
+            ret,buffer = cv2.imencode('.jpg',frame)
+    
+    
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -11,3 +20,6 @@ def index():
 @app.route('/video')
 def video():
     return Response(func(),mimetype= 'multipart/x-mixed-replace;boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
