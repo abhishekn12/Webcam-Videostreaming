1867363bbb0afbc3e28e422b46a58b9b3661bad6
Abhishek Nagesh <abhishek.n@daimler.com>
2021-06-14 23:50:03 +0530

    ready

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ def generate_frames():
             break
         else:
             ret,buffer = cv2.imencode('.jpg',frame)
+            frame =buffer.tobytes()
+        yield(b'--frame\r\n' 
+                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            
     
     
 @app.route('/')
@@ -19,7 +23,7 @@ def index():
 
 @app.route('/video')
 def video():
-    return Response(func(),mimetype= 'multipart/x-mixed-replace;boundary=frame')
+    return Response(generate_frames(),mimetype= 'multipart/x-mixed-replace;boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
